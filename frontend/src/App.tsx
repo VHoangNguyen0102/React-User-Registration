982f0181d756c3248ec536e77569eec3c6041ed9
Võ Hoàng Nguyên <nvo010204@gmail.com>
2025-10-29 22:15:58 +0700

    UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -7,11 +7,11 @@ import SignUp from './pages/SignUp';
 function App() {
   return (
     <Router>
-      <nav className="p-4 flex gap-4 bg-gray-100 justify-end">
+      {/* <nav className="p-4 flex gap-4 bg-gray-100 justify-end">
         <Link to="/home">Home</Link>
         <Link to="/login">Login</Link>
         <Link to="/signup">Sign Up</Link>
-      </nav>
+      </nav> */}
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/home" element={<Home />} />
